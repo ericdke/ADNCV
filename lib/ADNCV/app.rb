d0f9e11a4fd90b68f70c19a6d9cdf90a4a402a14
Eric Dejonckheere <eric@aya.io>
2014-12-26 18:21:23 +0100

    desc

--- lib/ADNCV/app.rb
+++ lib/ADNCV/app.rb
@@ -1,19 +1,19 @@
 # encoding: utf-8
 module ADNCV
   class App < Thor
-    package_name "ADNCV"
+    package_name "adncv"
     require_relative "display"
     require_relative "data"
 
-    desc "display", "Extract"
+    desc "display", "Show your account statistics (-d)"
     map "-d" => :display
-    option :full, aliases: "-f", type: :boolean, desc: "Display full details"
+    option :full, aliases: "-f", type: :boolean, desc: "Display full details: links, users, etc"
     def display(file)
       analyze(file)
       @display.show(@data, options)
     end
 
-    desc "export", "Export"
+    desc "export", "Export your account statistics (-e)"
     map "-e" => :export
     def export(file)
       analyze(file)
